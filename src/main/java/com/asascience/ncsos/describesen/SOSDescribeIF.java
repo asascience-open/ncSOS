cf13b027528b6c29aaab3a4bdb3d5767c3da569f
CowanSM <cowansm@gmail.com>
2012-07-03 15:31:06 -0400

    disseminated functions to subclasses/interfaces to prevent bloating of higher-level classes

---
+++ src/main/java/com/asascience/ncsos/describesen/SOSDescribeIF.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.asascience.ncsos.describesen;
+
+import com.asascience.ncsos.outputformatter.DescribeSensorFormatter;
+
+/**
+ *
+ * @author SCowan
+ */
+public interface SOSDescribeIF {
+    public void SetupOutputDocument(DescribeSensorFormatter output);
+}
