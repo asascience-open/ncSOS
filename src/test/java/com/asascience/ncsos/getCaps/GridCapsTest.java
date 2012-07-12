65afcfe259c6cde2331aeb06514789f9674ec1e6
CowanSM <cowansm@gmail.com>
2012-07-12 11:06:12 -0400

    random tweaks/fixes

--- src/test/java/com/asascience/ncsos/getCaps/GridCapsTest.java
+++ src/test/java/com/asascience/ncsos/getCaps/GridCapsTest.java
@@ -1,163 +1,145 @@
-///*
-// * To change this template, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package thredds.server.sos.getCaps;
-//
-//import java.io.BufferedWriter;
-//import java.io.CharArrayWriter;
-//import java.io.File;
-//import java.io.FileWriter;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
-//import ucar.nc2.ft.FeatureDataset;
-//import ucar.nc2.ft.FeatureDatasetFactoryManager;
-//import ucar.nc2.constants.FeatureType;
-//import java.util.Formatter;
-//import javax.swing.JOptionPane;
-//import ucar.nc2.thredds.ThreddsDataFactory;
-//import java.io.IOException;
-//import java.io.Writer;
-//import thredds.server.sos.util.DatasetHandlerAdapter;
-//import ucar.nc2.dataset.NetcdfDataset;
-//import org.junit.AfterClass;
-//import org.junit.BeforeClass;
-//import org.junit.Test;
-//import thredds.server.sos.service.SOSParser;
-//import thredds.server.sos.service.SOSBaseRequestHandler;
-//import ucar.nc2.util.CancelTask;
-//import static org.junit.Assert.*;
-//import ucar.unidata.geoloc.LatLonRect;
-//
-///**
-// *
-// * @author abird
-// */
-//public class GridCapsTest {
-//
-//    public static final String base = "tests/main/java/thredds/server/sos/getCaps/output/";
-//
-//    private static void fileWriter(String base, String fileName, Writer write) throws IOException {
-//        Writer output = null;
-//        File file = new File(base + fileName);
-//        output = new BufferedWriter(new FileWriter(file));
-//        output.write(write.toString());
-//        output.close();
-//        System.out.println("Your file has been written");
-//    }
-//
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thredds.server.sos.getCaps;
+
+import com.asascience.ncsos.outputformatter.SOSOutputFormatter;
+import com.asascience.ncsos.service.SOSParser;
+import com.asascience.ncsos.util.XMLDomUtils;
+import java.io.*;
+import java.util.HashMap;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import ucar.nc2.dataset.NetcdfDataset;
+
+/**
+ *
+ * @author abird
+ */
+public class GridCapsTest {
+
+    private static String baseLocalDir = null;
+    private static String outputDir = null;
+    
+    private static final String baseRequest = "request=GetCapabilities&version=1.0.0&service=sos";
+    
+    private static final String testGetCapsSST1 = "resources/datasets/satellite-sst/SST_Global_2x2deg_20120626_0000.nc";
+    private static final String testGetCapsSST2 = "resources/datasets/satellite-sst/SST_Global_2x2deg_20120627_0000.nc";
+
+    @BeforeClass
+    public static void SetupEnviron() {
+        // not really a test, just used to set up the various string values
+        if (outputDir != null && baseLocalDir != null) {
+            // exit early if the environ is already set
+            return;
+        }
+        String container = "getCaps";
+        InputStream templateInputStream = null;
+        try {
+            File configFile = new File("resources/tests_config.xml");
+            templateInputStream = new FileInputStream(configFile);
+            Document configDoc = XMLDomUtils.getTemplateDom(templateInputStream);
+            // read from the config file
+            outputDir = XMLDomUtils.getNodeValue(configDoc, container, "outputBase");
+            baseLocalDir = XMLDomUtils.getNodeValue(configDoc, container, "projectDir");
+        } catch (FileNotFoundException fnfex) {
+            System.out.println(fnfex.getMessage());
+        } finally {
+            if (templateInputStream != null) {
+                try {
+                    templateInputStream.close();
+                } catch (IOException e) {
+                    // ignore, closing..
+                }
+            }
+        }
+        
+        File file = new File(outputDir);
+        file.mkdirs();
+    }
+    
+    private static void fileWriter(String base, String fileName, Writer write) {
+        try {
+            File file = new File(base + fileName);
+            Writer output = new BufferedWriter(new FileWriter(file));
+            output.write(write.toString());
+            output.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    private static String getCurrentMethod() {
+        final StackTraceElement[] ste = Thread.currentThread().getStackTrace();
+        for (int i=0; i<ste.length; i++) {
+            if (ste[i].getMethodName().contains(("test")))
+                return ste[i].getMethodName();
+        }
+        return "could not find test name";
+    }
+    
+    private void writeOutput(HashMap<String, Object> outMap, Writer write) {
+        SOSOutputFormatter output = (SOSOutputFormatter)outMap.get("outputHandler");
+        assertNotNull("got null output", output);
+        output.writeOutput(write);
+    }
+
+    @Test
+    public void testParseSST1() {
+        System.out.println("\n------" + getCurrentMethod() + "------");
+        
+        try {
+            NetcdfDataset dataset = NetcdfDataset.openDataset(testGetCapsSST1);
+            SOSParser md = new SOSParser();
+            Writer writer = new CharArrayWriter();
+            writeOutput(md.enhance(dataset, baseRequest, testGetCapsSST1),writer);
+            writer.flush();
+            writer.close();
+            String fileName = "getCapsSST1.xml";
+            fileWriter(outputDir, fileName, writer);
+            assertFalse(writer.toString().contains("Exception"));
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            System.out.println("------END " + getCurrentMethod() + "------");
+        }
+    }
+    
+    @Test
+    public void testParseSST2() {
+        System.out.println("\n------" + getCurrentMethod() + "------");
+        
+        try {
+            NetcdfDataset dataset = NetcdfDataset.openDataset(testGetCapsSST2);
+            SOSParser md = new SOSParser();
+            Writer writer = new CharArrayWriter();
+            writeOutput(md.enhance(dataset, baseRequest, testGetCapsSST2),writer);
+            writer.flush();
+            writer.close();
+            String fileName = "getCapsSST2.xml";
+            fileWriter(outputDir, fileName, writer);
+            assertFalse(writer.toString().contains("Exception"));
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            System.out.println("------END " + getCurrentMethod() + "------");
+        }
+    }
+    
 //    @Test
-//    public void testCanIdentifyGriddedCDM() throws IOException {
-//        NetcdfDataset dataset = NetcdfDataset.openDataset("D:/Data/20120417.108.1357.n16.EC1.nc");
-//        SOSGetCapabilitiesRequestHandler sosget = new SOSGetCapabilitiesRequestHandler(dataset, "threddsURI");
-//        assertEquals(FeatureType.GRID, sosget.getDatasetFeatureType());
+//    public void testInsertClassNameHere() {
+//        System.out.println("\n------" + getCurrentMethod() + "------");
+//        
+//        try {
+//            
+//        } catch (IOException ex) {
+//            System.out.println(ex.getMessage());
+//        } finally {
+//            System.out.println("------END " + getCurrentMethod() + "------");
+//        }
 //    }
-//
-//    @Test
-//    public void testParseGriddedCDM() throws IOException {
-//        NetcdfDataset dataset = NetcdfDataset.openDataset("D:/Data/20120417.108.1357.n16.EC1.nc");
-//        SOSParser md = new SOSParser();
-//        Writer write = new CharArrayWriter();
-//        md.enhance(dataset, write, "request=GetCapabilities&version=1&service=sos", "D:/Data/20120417.108.1357.n16.EC1.nc");
-//        write.flush();
-//        write.close();
-//        assertFalse(write.toString().contains("Exception"));
-//        String fileName = "gridded.xml";
-//        fileWriter(base, fileName, write);
-//        assertTrue(write.toString().contains("<ObservationOffering gml:id="));
-//    }
-//
-//    @Test
-//    public void testParseGriddedHFRadarCDM() throws IOException {
-//        NetcdfDataset dataset = NetcdfDataset.openDataset("D:/Data/201204201200_HFRadar_USWC_6km_rtv_NDBC.nc");
-//        SOSParser md = new SOSParser();
-//        Writer write = new CharArrayWriter();
-//        md.enhance(dataset, write, "request=GetCapabilities&version=1&service=sos", "D:/Data/201204201200_HFRadar_USWC_6km_rtv_NDBC.nc");
-//        write.flush();
-//        write.close();
-//        assertFalse(write.toString().contains("Exception"));
-//        String fileName = "griddedHF.xml";
-//        fileWriter(base, fileName, write);
-//        assertTrue(write.toString().contains("<ObservationOffering gml:id="));
-//    }
-//
-//    @Test
-//    public void testHFRadarUExistsInGetCaps() throws IOException {
-//        NetcdfDataset dataset = NetcdfDataset.openDataset("D:/Data/201204201200_HFRadar_USWC_6km_rtv_NDBC.nc");
-//        SOSParser md = new SOSParser();
-//        Writer write = new CharArrayWriter();
-//        md.enhance(dataset, write, "request=GetCapabilities&version=1&service=sos", "D:/Data/201204201200_HFRadar_USWC_6km_rtv_NDBC.nc");
-//        write.flush();
-//        write.close();
-//        assertTrue(write.toString().contains("<ObservationOffering gml:id=\"u\">"));
-//    }
-//
-//    @Test
-//    public void testHFRadarVExistsInGetCaps() throws IOException {
-//        NetcdfDataset dataset = NetcdfDataset.openDataset("D:/Data/201204201200_HFRadar_USWC_6km_rtv_NDBC.nc");
-//        SOSParser md = new SOSParser();
-//        Writer write = new CharArrayWriter();
-//        md.enhance(dataset, write, "request=GetCapabilities&version=1&service=sos", "D:/Data/201204201200_HFRadar_USWC_6km_rtv_NDBC.nc");
-//        write.flush();
-//        write.close();
-//        assertTrue(write.toString().contains("<ObservationOffering gml:id=\"v\">"));
-//    }
-//
-//    @Test
-//    public void testHFRadarLatMinCorrect() throws IOException {
-//        NetcdfDataset dataset = NetcdfDataset.openDataset("D:/Data/201204201200_HFRadar_USWC_6km_rtv_NDBC.nc");
-//        SOSParser md = new SOSParser();
-//        Writer write = new CharArrayWriter();
-//        md.enhance(dataset, write, "request=GetCapabilities&version=1&service=sos", "D:/Data/201204201200_HFRadar_USWC_6km_rtv_NDBC.nc");
-//        write.flush();
-//        write.close();
-//        assertTrue(write.toString().contains("<gml:lowerCorner>30.25"));
-//    }
-//
-//    @Test
-//    public void testHFRadarLatMaxCorrect() throws IOException {
-//        NetcdfDataset dataset = NetcdfDataset.openDataset("D:/Data/201204201200_HFRadar_USWC_6km_rtv_NDBC.nc");
-//        SOSParser md = new SOSParser();
-//        Writer write = new CharArrayWriter();
-//        md.enhance(dataset, write, "request=GetCapabilities&version=1&service=sos", "D:/Data/201204201200_HFRadar_USWC_6km_rtv_NDBC.nc");
-//        write.flush();
-//        write.close();
-//        assertTrue(write.toString().contains("<gml:upperCorner>49.9920"));
-//    }
-//
-//    @Test
-//    public void testHFRadarLonMinCorrect() throws IOException {
-//        NetcdfDataset dataset = NetcdfDataset.openDataset("D:/Data/201204201200_HFRadar_USWC_6km_rtv_NDBC.nc");
-//        SOSParser md = new SOSParser();
-//        Writer write = new CharArrayWriter();
-//        md.enhance(dataset, write, "request=GetCapabilities&version=1&service=sos", "D:/Data/201204201200_HFRadar_USWC_6km_rtv_NDBC.nc");
-//        write.flush();
-//        write.close();
-//        assertTrue(write.toString().contains("-130.360"));
-//    }
-//
-//    @Test
-//    public void testHFRadarLonMaxCorrect() throws IOException {
-//        NetcdfDataset dataset = NetcdfDataset.openDataset("D:/Data/201204201200_HFRadar_USWC_6km_rtv_NDBC.nc");
-//        SOSParser md = new SOSParser();
-//        Writer write = new CharArrayWriter();
-//        md.enhance(dataset, write, "request=GetCapabilities&version=1&service=sos", "D:/Data/201204201200_HFRadar_USWC_6km_rtv_NDBC.nc");
-//        write.flush();
-//        write.close();
-//        assertTrue(write.toString().contains("-115.80556"));
-//    }
-//    
-//    @Test
-//    public void testDepthNCGriddedGetCaps() throws IOException {
-//          NetcdfDataset dataset = NetcdfDataset.openDataset("D:/Data/ncom_relo_amseas_u_2012041800_t036.nc");
-//        SOSParser md = new SOSParser();
-//        Writer write = new CharArrayWriter();
-//        md.enhance(dataset, write, "request=GetCapabilities&version=1&service=sos", "D:/Data/ncom_relo_amseas_u_2012041800_t036.nc");
-//        write.flush();
-//        write.close();
-//        assertFalse(write.toString().contains("Exception"));
-//        String fileName = "griddedDepth.xml";
-//        fileWriter(base, fileName, write);
-//        assertTrue(write.toString().contains("<ObservationOffering gml:id="));
-//    }
-//}
+
+}
